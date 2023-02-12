f20252b717ba73486b226eac5e38f905efb53a92
DullJZ <79080562+DullJZ@users.noreply.github.com>
2023-02-12 19:31:21 +0800

    删除无用函数

--- MyPvZInC/function.cpp
+++ MyPvZInC/function.cpp
@@ -136,21 +136,6 @@ int get_perfect_shoot_arg(int line) {
 	}
 }
 
-// 绘制爱心函数
-void heart(int x0, int y0, int size, COLORREF C)
-{
-	double  m, n, x, y; double i;
-	for (i = 0; i <= 2 * size; i = i + 0.01)
-	{
-		//产生极坐标点
-		m = i;
-		n = -size * (((sin(i) * sqrt(fabs(cos(i)))) / (sin(i) + 1.4142)) - 2 * sin(i) + 2);
-		//转换为笛卡尔坐标
-		x = n * cos(m) + x0;
-		y = n * sin(m) + y0;
-		putpixel(x, y, C);
-	}
-}
 
 // 给排行榜排序，返回最高分
 int sort() {
